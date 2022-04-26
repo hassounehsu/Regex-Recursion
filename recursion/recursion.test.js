effc51c5b79004a2d677e5e8f1963f1379db86e7
hassounehsu <hassouneh_sulaiman@yahoo.com>
2022-04-26 15:57:52 +0300

    done

--- recursion/recursion.test.js
+++ recursion/recursion.test.js
@@ -1,28 +1,26 @@
 /* Write a function to do the division operation without using the built-in division*/
 
-function division(number, dividedBy) {
-    if (number == 0 || dividedBy == 0) {
-        return 0
-    } else if (number < dividedBy) {
+function division(number, dividedBy){
+    if(dividedBy == 0){
         return 0;
-    } else {
-        return 1 + division(number - dividedBy, dividedBy);
-    }
-}
+        }
+let result = number/dividedBy;
+    return result;
 
+}
 /* Write a function that implement Math.pow(x,n) but using recursion
 Example:
 pow(2,4) = 16
 */
-
-
-function pow(x, n) {
-    if (n == 0) {
-        return 1
-    } else {
-        return (x * pow(x, n - 1));
-    }
+function pow(x,n){
+    if (n == 0)
+    return 1
+    if(x == 0) 
+    return 0
+    return x * (x*(n-1));
 }
+
+
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
 the next integer is determined by summing the previous two. This gives us:
 0, 1, 1, 2, 3, 5, 8, 13, ...
@@ -30,8 +28,10 @@ Write a function that take n as parameter and return the nth element in the Fibo
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
 function fibonacci(n){
-    // Write you logic here.
-    return;
+    if (n == 0 | n == 1)
+    return n;
+    else 
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
 /* Optional 
